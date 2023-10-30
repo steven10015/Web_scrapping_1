7ef6cca6e12e07e3e151c9fc981f2d5d34005989
Carlos MIjares <136937640+Carlos2607-a@users.noreply.github.com>
2023-10-30 16:52:18 +0000

    Realizado

--- src/app.py
+++ src/app.py
@@ -1,15 +1,57 @@
-import os
-from sqlalchemy import create_engine
 import pandas as pd
-from dotenv import load_dotenv
+import matplotlib.pyplot as plt 
+from bs4 import BeautifulSoup
+import requests
+import urllib.request
+import re
+import time
+from urllib.request import Request, urlopen
+import lxml
+import html5lib
+import sqlite3
 
-# load the .env file variables
-load_dotenv()
 
-# 1) Connect to the database here using the SQLAlchemy's create_engine function
+#Hacemos el request para acceder a la informacion y filtramos las tablas
+req = Request('https://www.macrotrends.net/stocks/charts/TSLA/tesla/revenue', headers={'User-Agent': 'XYZ/3.0'})
+webpage = urlopen(req, timeout=10).read()
+soup = BeautifulSoup(webpage, 'html.parser')
+tablas = soup.find_all('table')
 
-# 2) Execute the SQL sentences to create your tables using the SQLAlchemy's execute function
 
-# 3) Execute the SQL sentences to insert your data using the SQLAlchemy's execute function
+#Ubicamos la tabla que nos interesa en este caso 
+for indice, tabla in enumerate(tablas):
+    if ("Tesla Quarterly Revenue" in str(tablas)):
+        tablas.index = indice
+        break
 
-# 4) Use pandas to print one of the tables as dataframes using read_sql function
+#Leemos la tabla que nos interesa e indicamos que renombre las columnas
+resultado = pd.read_html(str(tablas))[1]
+tabladf =pd.DataFrame(resultado)
+tabladf = tabladf.rename(columns={"Tesla Quarterly Revenue(Millions of US $)": "Fecha", "Tesla Quarterly Revenue(Millions of US $).1": "Monto"})
+tabladf.replace('\$', '', regex=True, inplace=True)
+tabladf.head()
+
+#Validamos que no tenga valores vacios 
+tabladf.fillna(0, inplace=True)
+
+#Conectamos con sqlite
+connection = sqlite3.connect("tablatesla.db")
+
+#Creamos la Tabla
+cursor = connection.cursor()
+cursor.execute("""CREATE TABLE revenue (Fecha, Monto)""")
+
+#Transformamos los valores a tuplas 
+Tabla_tp = list(tabladf.to_records(index = False))
+Tabla_tp[:5]
+
+#Insertamos los Valores 
+cursor.executemany("INSERT INTO revenue VALUES (?,?)", Tabla_tp)
+connection.commit()
+
+#Cerramos Conexion
+connection.close()
+
+
+#Graficamos los valores
+
